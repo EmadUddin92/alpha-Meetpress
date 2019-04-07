fece987f8809a21cb36f3474edf46b8bc7ddbe42
Emad Uddin <emaduddin715@gmail.com>
2019-04-07 23:33:32 +0600

    few-code-changes

--- wp-content/themes/alpha/comments.php
+++ wp-content/themes/alpha/comments.php
@@ -1,3 +1,25 @@
+
+<div class="comments">
+
+<h2 class="comments-title">
+    
+    <?php
+    $alpha_cn = get_comments_number(); 
+    if(1==$alpha_cn){
+        _e("1 comments", "alpha");
+    }else  {
+        echo $alpha_cn."".__("Comments", "alpha");
+    }
+    ?>
+</h2>
+
 <?php
 wp_list_comments();
+if(! comments_open()){
+    _e("Comments are closed","alpha"); 
+}
+
 comment_form();
+?>
+</div>
+
--- wp-content/themes/alpha/single.php
+++ wp-content/themes/alpha/single.php
@@ -96,11 +96,13 @@ if (!is_active_sidebar("sidebar-1")) {
                                         </div>
                                     </div>
                                 </div>
+                                <?php if (! post_password_required() && comments_open()): ?>
                                 <div class="col-md-12">
                                     <?php
                                     comments_template();
                                     ?>
                                 </div>
+                                    <?php endif;  ?>
                             </div>
                         </div>
                     </div>
